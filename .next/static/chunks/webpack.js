ffef0d622494b81df0bdc04a10b08cb4c5f23fb5
maximuusS <ksaurav0211@gmail.com>
2022-01-18 19:35:24 +0530

    added routing

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3350d5ced8e6da57"; }
+/******/ 		__webpack_require__.h = function() { return "771b40417c998fca"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.4b9a8d7bc405ba63.hot-update.js
+++ .next/static/webpack/webpack.4b9a8d7bc405ba63.hot-update.js
@@ -40,7 +40,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "503a04cdaefe2796"; }
+/******/ 	__webpack_require__.h = function() { return "e365a85ebbc43c92"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/harmony module decorator */
